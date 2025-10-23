658658909fdbc2baf594bbf9b2f93517685f0d88
PerzivaL099 <mario152003@gmail.com>
2025-10-23 16:16:02 -0700

    Delete FinPrograma.java

--- FinPrograma.java
+++
@@ -1,9 +0,0 @@
-public class FinPrograma  extends Tupla{
-    public FinPrograma(){
-        super(-1, -1);
-    }
-
-    public String toString(){
-        return "( " + super.toString() + ", [], " + " )";
-    }
-}
